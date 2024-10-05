0bd19b2d0ed51782d79207d3089b97faf34d77aa
satyajitPatel <thriver1988@gmail.com>
2024-10-06 00:46:15 +0530

    add template

---
+++ template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<class T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// find_by_order(2) => iterator at index 2
+// order_of_key(2) => index of first occurance of element 2
+
+#define endl "\n"
+#define vi vector<int>
+#define vc vector<char>
+
+int main() {
+ // fast IO
+ ios::sync_with_stdio(false);
+ cin.tie(nullptr);
+ // ----------------------------------------------------------------------------
+ 
+ // ----------------------------------------------------------------------------
+ return 0;
+}
